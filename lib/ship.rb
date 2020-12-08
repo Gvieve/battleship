c43f019a1aa538f55ad8ad62f795f17455c35041
Gvieve <gnuebel84@yahoo.com>
2020-12-08 08:35:03 -0700

    Add ship class

---
+++ lib/ship.rb
@@ -0,0 +1,26 @@
+class Ship
+attr_reader :name,
+            :length
+
+  def initialize(name, length)
+    @name = name
+    @length = length
+    @health = length
+  end
+
+  def health
+    @health
+  end
+
+  def sunk?
+    if health == 0
+      true
+    else
+      false
+    end
+  end
+
+  def hit
+    @health -= 1
+  end
+end
---
+++ test/ship_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+  def test_it_exists_and_had_attributes
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_instance_of Ship, cruiser
+    assert_equal "Cruiser", cruiser.name
+    assert_equal 3, cruiser.length
+  end
+
+  def test_it_has_health
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal 3, cruiser.health
+  end
+
+  def test_if_it_is_sunk
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal false, cruiser.sunk?
+    cruiser.hit
+    assert_equal 2, cruiser.health
+    cruiser.hit
+    assert_equal 1, cruiser.health
+    cruiser.hit
+    assert_equal true, cruiser.sunk?
+  end
+end
