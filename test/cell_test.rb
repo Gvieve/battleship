96773c92142a52fe3c6229077d98c7799becba46
Caryn Keffer <carynkeffer@gmail.com>
2020-12-08 21:07:59 -0500

    Add Cell class and empty? and place_ship method

---
+++ test/cell_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+
+class CellTest < Minitest::Test
+  def test_it_exists_and_had_attributes
+    cell = Cell.new("B4")
+
+    assert_instance_of Cell, cell
+    assert_equal "B4", cell.coordinate
+    assert_equal nil, cell.ship
+    assert_equal true, cell.empty?
+  end
+
+  def test_cell_has_a_ship
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal true, cell.empty?
+    cell.place_ship(cruiser)
+    assert_equal false, cell.empty?
+  end
+end
