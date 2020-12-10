981046b5ab5afb44f9562833f7823c9b337b6974
Caryn Keffer <carynkeffer@gmail.com>
2020-12-10 17:09:57 -0500

    Add board class and valid_coordinate method

---
+++ test/board_test.rb
@@ -0,0 +1,34 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+class BoardTest < Minitest::Test
+  def test_it_exists_and_has_cells
+    board = Board.new
+
+    assert_instance_of Board, board
+    assert_equal 16, board.cells.keys.count
+  end
+
+  def test_cells_have_valid_coordinate
+    board = Board.new
+
+    assert_equal true, board.valid_coordinate?("A1")
+    assert_equal true, board.valid_coordinate?("D4")
+    assert_equal false, board.valid_coordinate?("A5")
+    assert_equal false, board.valid_coordinate?("E1")
+    assert_equal false, board.valid_coordinate?("A22")
+  end
+
+  def test_ship_has_valid_placement
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
+    assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
+    assert_equal true, board.consecutive_letters
+  end
+end
