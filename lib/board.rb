04b7497cdef94e911422be24c71bb5a2229da581
Caryn Keffer <carynkeffer@gmail.com>
2020-12-10 21:05:40 -0500

    Add coordinates_nums and coordinates_nums helper methods

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,8 @@
 class Board
-  attr_reader :cells
+  attr_reader :cells,
+              :coordinates
   def initialize
+    @coordinates = coordinates
     @cells = {
              "A1" => Cell.new("A1"),
              "A2" => Cell.new("A2"),
@@ -25,10 +27,6 @@ class Board
     @cells.has_key?(coordinate)
   end
 
-  def valid_placement?(ship, coordinates)
-    coordinates.count == ship.length
-  end
-
   def letters
     board_letters = []
     cells.keys.each do |letter|
@@ -38,14 +36,65 @@ class Board
   end
 
   def consecutive_letters
-    consecutive_letters = []
+    letters_validation = []
     letters.each_cons(3) do |letter|
-      consecutive_letters << letter
+      letters_validation << letter
     end
 
     letters.each_cons(2) do |letter|
-      consecutive_letters << letter
+      letters_validation << letter
     end
-    consecutive_letters
+    letters_validation
+  end
+
+  def numbers
+    board_numbers = []
+    cells.keys.each do |number|
+      board_numbers << number[1]
+    end
+    board_numbers.uniq
+  end
+
+  def consecutive_numbers
+    numbers_validation = []
+    numbers.each_cons(3) do |number|
+      numbers_validation << number
+    end
+
+    numbers.each_cons(2) do |number|
+      numbers_validation << number
+    end
+    numbers_validation
+  end
+
+  def valid_placement?(ship, coordinates)
+    # require "pry"; binding.pry
+    @coordinates = coordinates
+    (coordinates.count == ship.length) &&
+    (num_cons.include?(coordinates_nums))
+  end
+
+  def coordinates_ords
+    ords = []
+    coordinates.each do |coordinate|
+      ords << coordinate.chop.ord
+    end
+    ords.uniq
+  end
+
+  def coordinates_nums
+    nums = []
+    coordinates.each do |coordinate|
+      nums << coordinate[1..-1].to_i
+    end
+    nums.uniq
+  end
+
+  def num_cons
+    consecutive = []
+    (1..4).each_cons(coordinates.count) do |n|
+      consecutive << n
+    end
+    consecutive
   end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -29,8 +29,21 @@ class BoardTest < Minitest::Test
 
     assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
     assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
-    assert_equal ["A", "B", "C", "D"], board.letters
-    expected = [["A", "B", "C"], ["B", "C", "D"], ["A", "B"], ["B", "C"], ["C", "D"]]
-    assert_equal expected, board.consecutive_letters
+    # assert_equal ["A", "B", "C", "D"], board.letters
+    # expected = [["A", "B", "C"], ["B", "C", "D"], ["A", "B"], ["B", "C"], ["C", "D"]]
+    # assert_equal expected, board.consecutive_letters
+    # assert_equal ["1", "2", "3", "4"], board.numbers
+    # expected = [["1", "2", "3"], ["2", "3", "4"], ["1", "2"], ["2", "3"], ["3", "4"]]
+    # assert_equal expected, board.consecutive_numbers
+    # assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+    # assert_equal [65], board.coordinates_ords
+    assert_equal false, board.valid_placement?(submarine, ["A1", "B1", "D1"])
+    assert_equal [65, 66, 68], board.coordinates_ords
+
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
+    assert_equal [1, 2], board.coordinates_nums
+    
+    assert_equal [[1, 2], [2, 3], [3, 4]], board.num_cons
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
   end
 end
