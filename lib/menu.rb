3773a20fcf374c1b65a9fa40736169016f9fb750
Gvieve <gnuebel84@yahoo.com>
2020-12-13 12:07:52 -0700

    Add new methods for Menu to interact with user

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,10 +1,12 @@
 class Menu
   attr_reader :welcome,
-              :play
+              :play,
+              :board
 
   def initialize
     @welcome  = "Welcome to BATTLESHIP"
     @play     = "Enter p to play. Enter q to quit."
+    @board    = Board.new
   end
 
   def computer_ship_placed
@@ -17,5 +19,18 @@ class Menu
 
   def ship_units
     "The Cruiser is three units long and the Submarine is two units long."
+  end
+
+  def render
+    board.render
+  end
+
+  def enter_coords
+    "Enter the squares for the Cruiser (3 spaces):"
+  end
+
+  def gets_coords
+    coords = gets.chomp
+    coords.split(" ")
   end
 end
--- test/menu_test.rb
+++ test/menu_test.rb
@@ -1,8 +1,8 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-# require './lib/ship'
-# require './lib/cell'
-# require './lib/board'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
 require './lib/menu'
 
 class MenuTest < Minitest::Test
@@ -20,5 +20,17 @@ class MenuTest < Minitest::Test
     assert_equal "I have laid out my ships on the grid.", menu.computer_ship_placed
     assert_equal "You now need to lay out your two ships.", menu.lay_out
     assert_equal "The Cruiser is three units long and the Submarine is two units long.", menu.ship_units
+  end
+
+  def test_render_board_shows
+    menu = Menu.new
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    expected = "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n"
+
+    assert_equal expected, menu.render
+    assert_equal "Enter the squares for the Cruiser (3 spaces):", menu.enter_coords
+    # assert_equal [], menu.gets_coords
   end
 end
