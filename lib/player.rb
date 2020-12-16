e4186435d64abb681627275823d0d53511aed7a1
Gvieve <gnuebel84@yahoo.com>
2020-12-16 00:48:59 -0700

    Refector player class and instantiate boards and ships

--- lib/player.rb
+++ lib/player.rb
@@ -1,16 +1,11 @@
 class Player
-  attr_reader :board
-  def initialize(board)
+  attr_reader :board,
+              :cruiser,
+              :submarine
+
+  def initialize
     @board = Board.new
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
-  end
-
-  def cruiser(ship)
-    ship
-  end
-
-  def submarine(ship)
-    ship
   end
 end
